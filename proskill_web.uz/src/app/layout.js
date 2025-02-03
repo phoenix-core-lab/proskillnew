dc03878b6bbbd1ca14fdb31a83491dc8d36bcb8f
Anna <a.khripchenko@student.inha.uz>
2025-02-03 14:57:53 +0500

    add main config

--- proskill_web.uz/src/app/layout.js
+++ proskill_web.uz/src/app/layout.js
@@ -1,15 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Montserrat } from "next/font/google";
+import "./globals.sass";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const montserrat = Montserrat({
   subsets: ["latin"],
+  weight: ["400","500", "600", "700", "800"], 
+  variable: "--font-montserrat", 
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={montserrat.className}
       >
         {children}
       </body>
